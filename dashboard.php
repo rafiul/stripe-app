e9288e3a317b29d6d6c1c7e239309cb9e1682fc1
rafiul <bmrafiul.alam@gmail.com>
2025-08-18 20:42:07 +0600

    add inner wrap

--- dashboard.php
+++ dashboard.php
@@ -18,10 +18,10 @@ $stripe_account = get_stripe_account($_SESSION['user_id']);
 require_once __DIR__ . '/includes/header.php';
 ?>
 
-<div class="container-fluid">
+<div class="inner-wrap">
     <div class="row">
         <div class="custom-col-20">
-            <div class="card mb-4">
+            <div class="card">
                 <div class="card-body sidebar">
                     <h5 class="card-title">Navigation</h5>
                     <?php include __DIR__ . '/includes/nav.php'; ?>
