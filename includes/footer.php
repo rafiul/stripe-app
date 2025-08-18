082943725f2f9afc8e63551061104f0ba99c81f4
rafiul <bmrafiul.alam@gmail.com>
2025-08-18 22:12:17 +0600

    main wrap

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
     </main>
 
-    <footer class="py-4 mt-5">
+    <footer class="py-2 mt-5">
         <div class="container text-center">
             <small class="text-muted mb-0">&copy; <?php echo date('Y'); ?> <?php echo APP_NAME; ?>. All rights reserved.</small>
         </div>
