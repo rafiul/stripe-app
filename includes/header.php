c25df9a0487704dd668c412dbe21eb4786fb8ff3
rafiul <bmrafiul.alam@gmail.com>
2025-08-19 14:07:58 +0600

    onboard assets

--- includes/header.php
+++ includes/header.php
@@ -21,11 +21,12 @@ if (ob_get_level() > 0) {
     <title><?php echo APP_NAME; ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css">
-    <!--link rel="stylesheet" href="/assets/css/style.css"-->
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>/assets/style.css">
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <div class="container">
+
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
             </button>
