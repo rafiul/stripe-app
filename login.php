384193d650b123a568397f8c0e9da120383e6123
rafiul <bmrafiul.alam@gmail.com>
2025-08-20 17:03:01 +0600

    add remember options

--- login.php
+++ login.php
@@ -99,6 +99,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     <label>Password</label>
                     <input type="password" name="password" placeholder="Enter your password" required>
                 </div>
+                <div class="form-options">
+                    <label><input type="checkbox" name="remember"> Remember for 7 days</label>
+                </div>
                 <button type="submit" class="login-btn primary">Sign in</button>
             </form>
         </div>
